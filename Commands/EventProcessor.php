c66569ff378317db5e785686061c295a29d87b00
Kurt Zenisek <KZeni1@gmail.com>
2017-10-16 16:31:31 -0500

    Update EventProcessor.php
    Improved some of the wording for the comments & option description.

--- Commands/EventProcessor.php
+++ Commands/EventProcessor.php
@@ -47,13 +47,13 @@ class EventProcessor extends ConsoleCommand
     {
 	    $command->setName('aom:process');
 	    $command->setDescription('Processes visits and conversions by updating aom_visits.');
-	    $command->addOption('memory-limit', null, InputOption::VALUE_OPTIONAL, 'Forwards the PHP memory_limit value to the PHP CLI command. For example `--memory-limit=2147483648` would result in the process being allowed 2GB of RAM.', $default = '');
+	    $command->addOption('memory-limit', null, InputOption::VALUE_OPTIONAL, 'Forwards the PHP memory_limit value to the PHP CLI command. For example `--memory-limit=2147483648` would result in the process being allowed 2GB of RAM. Default is to not specify this value and use the server\'s own memory_limit value.', $default = '');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 
-        // We might need a little more RAM
+        // We might need a little more RAM than what the server's default memory_limit value is so check if the memory-limit option was specified.
         if($input->getOption('memory-limit')){ // Use what was provided via console/cli option
             ini_set('memory_limit',$input->getOption('memory-limit'));
         }
